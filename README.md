01c3edf937b26865a05fd38f1d8a89a7513bd2d2
dlb <dlb96@qq.com>
2018-04-01 12:42:33 +0800

    [src] init homepage

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Web-User-Interface
+
+## 开发环境
+
+vue-cli 使用webpack模板
+
+## 开发过程
+
+[2018/4/1] 首页原型
---
+++ e-tickets/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <router-view id="contents"></router-view>
+    <ul id="tags">
+      <li>
+        <!-- 使用router-link指向定义的path -->
+        <router-link :to="{name: 'Home'}">
+          热映
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'Home'}">
+          待定
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'Home'}">
+          待定
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'Mine'}">
+          我的
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html, body {
+  width: 100%;
+  height: 100%;
+  margin:0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column-reverse;
+  /* justify-content: */
+}
+
+#contents {
+  height: 90%;
+}
+
+#tags {
+  margin: 0;
+  padding: 0;
+  height: 10%;
+  display: flex;
+  justify-content: center;
+}
+
+li {
+  width:25%;
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
