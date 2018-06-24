c9376467d061e0712da06481842f4cbcce062605
dlb <dlb96@qq.com>
2018-06-24 14:20:23 +0800

    [src] signin page UI done

--- e-tickets/src/components/MovieDetailPage.vue
+++ e-tickets/src/components/MovieDetailPage.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div id="header">
-      <img src="../../static/img/chat3.png" alt="">
-      <span>发现</span>
-      <img src="../../static/img/search.png" alt="">
-    </div>
-    <div>
+    <div id="detail">
+      <div id="header">
+        <img src="../../static/img/chat3.png" alt="">
+        <span>发现</span>
+        <img src="../../static/img/search.png" alt="">
+      </div>
     </div>
   </div>
 </template>
@@ -22,4 +22,15 @@ export default {
 }
 </script>
 <style scope>
+#detail {
+  position: absolute;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  background: #bdc3c7;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #2c3e50, #bdc3c7);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #2c3e50, #bdc3c7); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  z-index:2;
+}
 </style>
--- e-tickets/src/components/SignInOnPage.vue
+++ e-tickets/src/components/SignInOnPage.vue
@@ -3,18 +3,18 @@
     <div id="sign">
       <img src="../../static/img/signin/title.png" alt="">
       <form>
-        <div>
+        <span>
           <img class="icon" src="../../static/img/signin/user.png" alt="">
           <input type="text" value="用户名">
-        </div>
-        <div>
+        </span>
+        <span>
           <img class="icon" src="../../static/img/signin/password.png" alt="">
           <input type="text" value="密码">
-        </div>
-        <input type="button" value="忘记密码">
+        </span>
+        <input id="forget" type="button" value="忘记密码?">
         <div>
-          <input type="button" value="登陆">
-          <input type="button" value="注册">
+          <input id="signin" type="button" value="登陆">
+          <input id="signup" type="button" value="注册">
         </div>
       </form>
     </div>
@@ -36,13 +36,14 @@
   display: flex;
   flex-direction: column;
   justify-content: center;
+  /* space-around; */
   align-items: center;
 }
 
 #sign>img{
-  width: 80%;
-  margin-top: 12.5%;
-  margin-bottom: 12.5%;
+  width: 60%;
+  /* margin-top: 12.5%; */
+  /* margin-bottom: 12.5%; */
 }
 
 .icon{
@@ -50,4 +51,63 @@
   height: 2rem;
 }
 
+form span{
+  box-sizing: border-box;
+  width: 30rem;
+  height: 4rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.5);
+  border:1px solid rgb(0, 102, 153);
+  border-radius: 1.7rem;
+  margin-top:5rem;
+}
+
+span>input{
+  color: rgb(102, 102, 102);
+  padding:1rem;
+  width:24rem;
+  background:rgba(255,255,255,0);
+  border:none;
+}
+
+span>input:focus{
+  outline:none;
+}
+
+#forget {
+  margin-top:1rem;
+  margin-bottom:1rem;
+  margin-left:70%;
+  background:none;
+  color: rgb(102, 102, 102);
+  border:none;
+}
+
+div>input {
+  margin:auto;
+  color:white;
+  box-sizing: border-box;
+  width:12rem;
+  height:4rem;
+  padding:none;
+  border:0;
+  border-radius:1.1rem;
+  font-size:2rem;
+}
+
+#signin{
+  background:rgb(0, 102, 153);
+}
+#signup{
+  background:rgb(204, 51, 0);
+}
+form div{
+  display:flex;
+  flex-direction:row;
+  justify-content: space-around;
+}
 </style>
