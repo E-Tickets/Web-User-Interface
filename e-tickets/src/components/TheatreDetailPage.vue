e9d6e32830e0fd15db552cb3cb79f307de61af22
dlb <dlb96@qq.com>
2018-06-30 01:02:24 +0800

    [src] theatre page modified

--- e-tickets/src/components/TheatreDetailPage.vue
+++ e-tickets/src/components/TheatreDetailPage.vue
@@ -4,17 +4,28 @@
       <div id="header">
         <img src="../../static/img/arrow.png" alt="" v-on:click="goBack()">
         <span>影院</span>
-          <img src="" alt="">
-        </div>
-          <span>{{name}}</span>
-          <hr>
-          <img src="../../static/img/location.png" alt="">
-        <div>
-
+        <img src="" alt="">
+      </div>
+      <div id="name">
+        <span>{{name}}</span>
+        <img src="../../static/img/location.png" alt="">
+      </div>
+      <div id="sale">
+        优惠信息
+      </div>
+      <div>
         <div id="date" >
           <span v-for="date in dates" v-bind:key="date.index">{{date.month}}月{{date.day}} {{date.describe}}</span>
         </div>
       </div>
+      <div id="schedules">
+          <div v-for="schedule in schedules" v-bind:key="schedule.index">
+            <span>{{schedule.time}}</span>
+            <span>国语3D</span>
+            <span>￥{{schedule.price}}</span>
+            <button>确定</button>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -22,23 +33,9 @@
 export default {
   data () {
     return {
-      dates: [
-        {
-          month: '6',
-          day: '25',
-          describe: '今天'
-        },
-        {
-          month: '6',
-          day: '26',
-          describe: '明天'
-        },
-        {
-          month: '6',
-          day: '27',
-          describe: '后天'
-        }
-      ]
+      name: '',
+      dates: [],
+      schedules: []
     }
   },
   methods: {
@@ -54,8 +51,35 @@ export default {
     this.$http.get(`api/schedules/movieid/${this.$route.params.mid}/cinemaid/${this.$route.params.tid}`)
       .then((data) => {
         console.log(data.body.data)
+        const theatre = data.body.data
+        this.name = theatre[0].cinema_name
+        theatre.forEach((schedule) => {
+          this.schedules.push({
+            time: schedule.time.slice(10, 16),
+            price: schedule.price
+          })
+        })
         // how to change the id to name
       })
+    const myDate = new Date()
+    this.dates.push({
+      month: myDate.getMonth() + 1,
+      day: myDate.getDate(),
+      describe: '今天'
+    })
+    myDate.setDate(myDate.getDate() + 1)
+    this.dates.push({
+      month: myDate.getMonth() + 1,
+      day: myDate.getDate(),
+      describe: '明天'
+    })
+    myDate.setDate(myDate.getDate() + 1)
+    this.dates.push({
+      month: myDate.getMonth() + 1,
+      day: myDate.getDate(),
+      describe: '后天'
+    })
+    console.log(this.dates)
   }
 }
 </script>
@@ -93,6 +117,57 @@ export default {
 #header>span{
   /* width:10%; */
   /* margin-left:40%; */
+}
+
+#name {
+  display:flex;
+  flex-direction: row;
+  font-size: 2rem;
+  font-weight: 600;
+  width:100%;
+  height:8%;
+}
+
+#name span{
+  box-sizing: border-box;
+  margin: 1rem;
+  width: 80%;
+  border-right: 0.2rem solid gray;
+}
+
+#sale {
+  box-sizing: border-box;
+  width:90%;
+  height:5%;
+  margin: 1rem 5% 1rem 5%;
+  font-size:1.75rem;
+  border: 0.1rem solid rgb(255, 102, 0);
+  color: rgb(255, 102, 0);
+  background-color: rgba(255, 204, 102, 0.65);
+  display:flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+
+#date {
+  display:flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 2rem;
+  width:100%;
+  height:6%;
+}
+
+#schedules div{
+  display:flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width:94%;
+  height:5%;
+  margin: 1rem 3% 1rem 3%;
 }
 
 </style>
